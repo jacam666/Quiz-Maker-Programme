27c2dade2f5d61670cd26c1ea25eab5db31efe9f
jacam666 <ja6cam@gmail.com>
2021-08-02 14:41:22 +0200

    Add project files.

---
+++ Quiz Maker Programme/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Quiz_Maker_Programme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string playerName;
+            string response;
+            Console.Write("Hi there please enter your name: ");
+            playerName = Console.ReadLine();
+            Console.WriteLine($"Welcome to my Quiz Game {playerName} are you ready to play? ");
+            
+            bool wantsToPlay = false;
+            Console.WriteLine("Play Quiz Game? yes/no");
+            response = Console.ReadLine();
+            if (response == "yes")
+            {
+                wantsToPlay = true;
+            }
+            
+            
+              
+            
+            
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
