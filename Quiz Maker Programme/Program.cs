77d832f51efe9d1bb0f968c779b1d813203bf30d
jacam666 <ja6cam@gmail.com>
2021-08-02 22:51:54 +0200

    : just testing how to get the questions and the answers to work together while its being chosen by random : added a while loop for just one question at the moment to see if it will work

--- Quiz Maker Programme/Program.cs
+++ Quiz Maker Programme/Program.cs
@@ -12,8 +12,19 @@ namespace Quiz_Maker_Programme
             UI.DecisionToPlay();
             UI.QuizQuestions();
             UI.QuestionRandomiser();
-
-            answers MultipleAnswers = new();
+            int attempts = 10;
+            Program.Questions QuizQuestions = new();
+            QuizQuestions.FirstQuestion = "What country won the very first fifa world cup?";
+            QuizQuestions.SecondQuestion = "What year was the very first fifa world cup?";
+            QuizQuestions.ThirdQuestion = "Whats the shortcut for the 'copy' function in most computers?";
+            QuizQuestions.ForthQuestion = "What was Twitters original name?";
+            QuizQuestions.FifthQuestion = "Which animal can be seen in the Porsche logo?";
+            QuizQuestions.SixthQuestion = "What is the common name for dried plums?";
+            QuizQuestions.SeventhQuestion = "What is the primary ingredient in hummus?";
+            QuizQuestions.EighthQuestion = "Which country produces the most coffee in the world?";
+            QuizQuestions.NinthQuestion = "Which country is responsible for giving us pizza and pasta?";
+            QuizQuestions.TenthQuestion = "Which organ has four chambers?";
+            Answers MultipleAnswers = new();
             MultipleAnswers.FirstQuestionAnswers = "A.England, B.Germany, C.Italy, or D.Uruguay";
             MultipleAnswers.SecondQuestionAnswers = "A.1930, B.1918, C.1934, or D.1938.";
             MultipleAnswers.ThirdQuestionAnswers = "A.ctrl a, B.ctrl 9, C.ctrl c, or D.ctrl s.";
@@ -24,6 +35,25 @@ namespace Quiz_Maker_Programme
             MultipleAnswers.EighthQuestionAnswers = "A.Columbia, B.USA, C.Brazil, or D.Mexico.";
             MultipleAnswers.NinthQuestionAnswers = "A.England, B.Poland, C.Italy, or D.Russia.";
             MultipleAnswers.TenthQuestionAnswers = "A.Heart, B.Lungs, C.Liver, or D.Kidneys.";
+
+            while (attempts > 0)
+            {
+                Console.WriteLine("Q1):" + QuizQuestions.FirstQuestion + " " + MultipleAnswers.FirstQuestionAnswers);
+                string Answer = Console.ReadLine();
+                string Correct = "D";
+                if (Answer == Correct)
+                {
+                    Console.WriteLine("Congrats that answer is correct");
+                }
+                else
+                {
+                    Console.WriteLine("Unlucky that answer is incorrect");
+                }
+                
+            }
+
+
+
         }
         public class Questions
         {
@@ -39,7 +69,7 @@ namespace Quiz_Maker_Programme
             public string TenthQuestion;
 
         }
-        public class answers
+        public class Answers
         {
             public string FirstQuestionAnswers;
             public string SecondQuestionAnswers;
