7dba77a11ca93e549a0fcc9718c01340ec9200db
jacam666 <ja6cam@gmail.com>
2021-08-02 21:06:07 +0200

    : changed the questionrandomiser to a list

--- Quiz Maker Programme/Program.cs
+++ Quiz Maker Programme/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Quiz_Maker_Programme
 {
@@ -23,8 +24,23 @@ namespace Quiz_Maker_Programme
             QuizQuestions.TenthQuestion = "Which organ has four chambers?";
             //Console.WriteLine(QuizQuestions.TenthQuestion);
             //Console.ReadLine();
-            UI.QuestionRandomiser();
-            
+            //UI.QuestionRandomiser();
+            List<string> Questions = new();
+            Questions.Add("What counry won the very first fifa world cup ? ");
+            Questions.Add("What year was the very first fifa world cup?");
+            Questions.Add("Whats the shortcut for the 'copy' function in most computers?");
+            Questions.Add("What was Twitters original name?");
+            Questions.Add("Which animal can be seen in the Porsche logo?");
+            Questions.Add("What is the common name for dried plums?");
+            Questions.Add("What is the primary ingredient in hummus?");
+            Questions.Add("Which country produces the most coffee in the world?");
+            Questions.Add("Which country is responsible for giving us pizza and pasta?");
+            Questions.Add("Which organ has four chambers?");
+            Random rnd = new();
+            int randomQuest = rnd.Next(Questions.Count);
+            Console.WriteLine($"Your question is : {Questions[randomQuest]}");
+
+
         }
         public class Questions
         {
