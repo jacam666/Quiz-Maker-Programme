97bc803be23dbb2876aba642a85e202aa9e25217
jacam666 <ja6cam@gmail.com>
2021-08-02 19:58:43 +0200

    :radomised the questions

--- Quiz Maker Programme/Program.cs
+++ Quiz Maker Programme/Program.cs
@@ -19,11 +19,18 @@ namespace Quiz_Maker_Programme
             QuizQuestions.SixthQuestion = "What is the common name for dried plums?";
             QuizQuestions.SeventhQuestion = "What is the primary ingredient in hummus?";
             QuizQuestions.EighthQuestion = "Which country produces the most coffee in the world?";
-            QuizQuestions.NinthQuestion = "Which country is responsible for giving us pizza and paste?";
+            QuizQuestions.NinthQuestion = "Which country is responsible for giving us pizza and pasta?";
             QuizQuestions.TenthQuestion = "Which organ has four chambers?";
+            //Console.WriteLine(QuizQuestions.TenthQuestion);
+            //Console.ReadLine();
+            string[] Questions = { "What counry won the very first fifa world cup?", "What year was the very first fifa world cup?", "Whats the shortcut for the 'copy' function in most computers?", "What was Twitters original name?",
+            "Which animal can be seen in the Porsche logo?", "What is the common name for dried plums?", "What is the primary ingredient in hummus?", "Which country produces the most coffee in the world?",
+            "Which country is responsible for giving us pizza and pasta?", "Which organ has four chambers?"};
+
+            Random rnd = new ();
+            int index = rnd.Next(Questions.Length);
+            Console.WriteLine($"Your question is : {Questions[index]}");
             
-
-
         }
         public class Questions
         {
