8f599d55459e05f8efaaa92bec92ce2966c1eca4
jacam666 <ja6cam@gmail.com>
2021-08-02 20:02:16 +0200

    : added ui method for randomising the quiz questions

--- Quiz Maker Programme/UI.cs
+++ Quiz Maker Programme/UI.cs
@@ -37,5 +37,16 @@ namespace Quiz_Maker_Programme
             }
             return wantsToPlay;
         }
+
+        public static void QuestionRandomiser()
+        {
+            string[] Questions = { "What counry won the very first fifa world cup?", "What year was the very first fifa world cup?", "Whats the shortcut for the 'copy' function in most computers?", "What was Twitters original name?",
+            "Which animal can be seen in the Porsche logo?", "What is the common name for dried plums?", "What is the primary ingredient in hummus?", "Which country produces the most coffee in the world?",
+            "Which country is responsible for giving us pizza and pasta?", "Which organ has four chambers?"};
+
+            Random rnd = new();
+            int index = rnd.Next(Questions.Length);
+            Console.WriteLine($"Your question is : {Questions[index]}");
+        }
     }
 }
