fc44fdece0565e654a992c275f2cdb1792a67414
jacam666 <ja6cam@gmail.com>
2021-08-02 15:50:18 +0200

    : added questions class

--- Quiz Maker Programme/Program.cs
+++ Quiz Maker Programme/Program.cs
@@ -6,11 +6,21 @@ namespace Quiz_Maker_Programme
     {
         static void Main(string[] args)
         {
-
             UI.DisplayWelcomeMessage();
             UI.DecisionToPlay();
             
 
+
+        }
+        public class Questions
+        {
+            public string FirstQuestion;
+            public string SecondQuestion;
+            public string ThirdQuestion;
+            public string ForthQuestion;
+            public string FifthQuestion;
+            public string SixthQuestion;
+            public string SeventhQuestion;
 
         }
     }
