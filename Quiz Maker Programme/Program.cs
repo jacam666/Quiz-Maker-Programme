b86c35307df28088f7693f0944856b3deb6cc090
jacam666 <ja6cam@gmail.com>
2021-08-22 12:38:12 +0100

    : added new variables

--- Quiz Maker Programme/Program.cs
+++ Quiz Maker Programme/Program.cs
@@ -11,25 +11,23 @@ namespace Quiz_Maker_Programme
         {
             //UI.DisplayWelcomeMessage();
             //UI.DecisionToPlay();
+
+            string[] Lines = System.IO.File.ReadAllLines(@"C:\Users\ja6ca\source\repos\Quiz Maker Programme\Quiz Maker Programme\readalllines questions and answers.txt");
+
+            for (int i = 0; i < 5; i++)
             {
-
-                string[] Lines = System.IO.File.ReadAllLines(@"C:\Users\ja6ca\source\repos\Quiz Maker Programme\Quiz Maker Programme\readalllines questions and answers.txt");
-
-                for (int i = 0; i < 5; i++)
-                {                   
-                    Console.WriteLine(Lines[i]);                  
-                }
-
-                
+                var gc = new QuestionAndAnswer();
+                gc.Question = Lines[i];
+                gc.Answers.Add(Lines[i + 1]);
+                Console.WriteLine(gc.Question);
             }
+            Console.ReadLine();
 
             ///go through every line of thet file (you'll get those as strings) File.ReadAllLines()
             ///for each question / answer / etc
             ///create a new gamecard, 
             ///add the strings you gort from the file to the gamecard
             ///add this gamecard to your list
-
-
             List<QuestionAndAnswer> GameCards = new();
 
             //GameCards.Add(GameCard1);
@@ -42,8 +40,9 @@ namespace Quiz_Maker_Programme
             //GameCards.Add(GameCard8);
             //GameCards.Add(GameCard9);
             //GameCards.Add(GameCard10);
+            
 
-            QuestionAndAnswer theLastcard = GameCards.Find(g => g.CorrectAnswer == "B");
+            //QuestionAndAnswer theLastcard = GameCards.Find(g => g.CorrectAnswer == "B");
 
             Random rnd = new();
             List<QuestionAndAnswer> randomlyOrderdQuestions = GameCards.OrderBy(g => rnd.Next()).ToList();
@@ -69,33 +68,6 @@ namespace Quiz_Maker_Programme
                     }
                 }
             }
-
-
-            //int attempts = 10;
-            //Questions QuizQuestions = new();
-            //QuizQuestions.FirstQuestion = "What country won the very first fifa world cup?";
-            //QuizQuestions.SecondQuestion = "What year was the very first fifa world cup?";
-            //QuizQuestions.ThirdQuestion = "Whats the shortcut for the 'copy' function in most computers?";
-            //QuizQuestions.ForthQuestion = "What was Twitters original name?";
-            //QuizQuestions.FifthQuestion = "Which animal can be seen in the Porsche logo?";
-            //QuizQuestions.SixthQuestion = "What is the common name for dried plums?";
-            //QuizQuestions.SeventhQuestion = "What is the primary ingredient in hummus?";
-            //QuizQuestions.EighthQuestion = "Which country produces the most coffee in the world?";
-            //QuizQuestions.NinthQuestion = "Which country is responsible for giving us pizza and pasta?";
-            //QuizQuestions.TenthQuestion = "Which organ has four chambers?";
-
-            //Answers MultipleAnswers = new();
-            //MultipleAnswers.FirstQuestionAnswers = "A.England, B.Germany, C.Italy, or D.Uruguay";
-            //MultipleAnswers.SecondQuestionAnswers = "A.1930, B.1918, C.1934, or D.1938.";
-            //MultipleAnswers.ThirdQuestionAnswers = "A.ctrl a, B.ctrl 9, C.ctrl c, or D.ctrl s.";
-            //MultipleAnswers.ForthQuestionAnswers = "A.Twitter, B.Twitch, C.Twicker, or D.instaTwit.";
-            //MultipleAnswers.FifthQuestionAnswers = "A.lion, B.Horse, C.eagle, or D.Dolphin.";
-            //MultipleAnswers.SixthQuestionAnswers = "A.prunes, B.Raisins, C.sultana's, or D.Dates.";
-            //MultipleAnswers.SeventhQuestionAnswers = "A.Rice, B.ChickPeas, C.Beans, or D.Egg.";
-            //MultipleAnswers.EighthQuestionAnswers = "A.Columbia, B.USA, C.Brazil, or D.Mexico.";
-            //MultipleAnswers.NinthQuestionAnswers = "A.England, B.Poland, C.Italy, or D.Russia.";
-            //MultipleAnswers.TenthQuestionAnswers = "A.Heart, B.Lungs, C.Liver, or D.Kidneys.";
-
             //while (attempts > 0)
             //{
             //    Console.WriteLine("Q1):" + QuizQuestions.FirstQuestion + " " + MultipleAnswers.FirstQuestionAnswers);
@@ -118,15 +90,6 @@ namespace Quiz_Maker_Programme
             //}
 
         }
-        
-        
-
-        
-        
-
-
-
-
 
         //public class Questions
         //{
